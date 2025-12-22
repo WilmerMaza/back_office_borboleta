6a6f8d574b862afcbd73fca618f9246837cb2d44
Ale13258 <aleja13258@gmail.com>
2025-12-22 16:05:35 -0500

    fix: update API URLs to point to production server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   URLS: "assets/data", // Change only the domain part, keeping "/api/admin" intact
   storageURL: "assets",
-  URL:"http://localhost:3001/api",
+  URL:"https://borboleta.site/api",
   URLs: "https://borboleta.site/api",
 };
